623798f4247533b3934ca5918d0a5788edc87217
Duaax24 <167181641+Duaax24@users.noreply.github.com>
2026-05-19 11:05:44 +0300

    fix

--- server.js
+++ server.js
@@ -6,12 +6,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Scores speichern
+let scores = [];
+
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Backend läuft!");
+});
+
+// SCORE SPEICHERN
+app.post("/score", (req, res) => {
+  const { name, score } = req.body;
+
+  scores.push({
+    name,
+    score
+  });
+
+  // Nach Score sortieren
+  scores.sort((a, b) => b.score - a.score);
+
+  res.json({
+    success: true
+  });
+});
+
+// LEADERBOARD LADEN
+app.get("/leaderboard", (req, res) => {
+  res.json(scores);
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server läuft");
+  console.log("Server läuft auf Port " + PORT);
 });
