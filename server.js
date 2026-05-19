02738a5f9181c694ee2bbcea7eff86181cce0dc9
Duaax24 <167181641+Duaax24@users.noreply.github.com>
2026-05-19 12:36:40 +0300

    fix

--- server.js
+++ server.js
@@ -15,29 +15,4 @@ app.get("/", (req, res) => {
 });
 
 // SCORE SPEICHERN
-app.post("/score", (req, res) => {
-  const { name, score } = req.body;
-
-  scores.push({
-    name,
-    score
-  });
-
-  // Nach Score sortieren
-  scores.sort((a, b) => b.score - a.score);
-
-  res.json({
-    success: true
-  });
-});
-
-// LEADERBOARD LADEN
-app.get("/leaderboard", (req, res) => {
-  res.json(scores);
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server läuft auf Port " + PORT);
-});
+app.post("/score", (req, res) 
